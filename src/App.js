2028d2512dc7bfa686ec9800b74b1f4fabe2b04c
krissbec <krissbec@xal.no>
2021-10-08 10:14:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bye there you and me and us!
         </p>
         <a
           className="App-link"
